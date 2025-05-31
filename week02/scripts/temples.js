ecf8439b21c5ce1db8799e0fdeef2f144e7fcf53
hernanortega1994 <hernanortega1994@gmail.com>
2025-05-31 09:50:36 -0500

    Album

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,3 +1,11 @@
 const yearSpan = document.getElementById("currentYear");
 yearSpan.textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const title = document.querySelector('.title')
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    title.classList.toggle('hide');
+});
